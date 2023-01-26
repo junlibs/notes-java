9540b194600f614b5de27e1f9aea869c248ac925
WangJun <843933709@qq.com>
2023-01-26 15:55:35 +0800

    java程序的注释

--- src/chapter01/demo02/Comments.java
+++ src/chapter01/demo02/Comments.java
@@ -3,7 +3,7 @@ package chapter01.demo02;
 /**
  * @author https://github.com/junlibs
  * @date 2023-01-13
- * @Description java程序的注释。此处就是文档注释，用来说明程序作用，在方法上也可以添加文档注释
+ * @Description java程序的注释有三种，单行注释。多行注释，文档注释。此处就是文档注释，用来说明程序作用，在方法上也可以添加文档注释
  */
 public class Comments {
     public static void main(String[] args) {
